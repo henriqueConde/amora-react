515c7eecf6c9c3cf28ed8902d598401006af94e8
Henrique Conde <ext.henrique.conde@farfetch.com>
2022-09-16 21:06:10 +0100

    feat: create banner component

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,23 @@
+import { Story, Meta } from '@storybook/react'
+import Banner, { BannerProps } from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner,
+  args: {
+    img: 'https://images.unsplash.com/photo-1523293182086-7651a899d37f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    title: 'Deep Blue Dive',
+    subtitle: '<p>Experience the new <strong>Blue Wave</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/perfume/deep-blue-dive'
+  },
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: Story<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Banner {...args} />
+  </div>
+)
---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,34 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+const props = {
+  img: 'https://images.unsplash.com/photo-1523293182086-7651a899d37f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+  title: 'Deep Blue Dive',
+  subtitle: '<p>Experience the new <strong>Blue Wave</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/perfume/deep-blue-dive'
+}
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Banner {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Deep Blue Dive/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /Experience the new Blue Wave season/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('img', { name: /Deep Blue Dive/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
