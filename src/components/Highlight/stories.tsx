755d77307816120b6d1f3ad0361f2f78f28e36f4
Henrique Conde <ext.henrique.conde@farfetch.com>
2022-09-16 23:19:13 +0100

    feat: create highlight component

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,30 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Elyseé is back!',
+    subtitle: 'Come see the new fragrance from Elyseé',
+    backgroundImage: '/img/highlight-img-1.jpg',
+    buttonLabel: 'Buy now',
+    buttonLink: '/elyseé/new-fragrance'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+WithFloatImage.args = {
+  floatImage: '/img/perfume-float-image-1.png'
+}
