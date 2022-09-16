31075ddfeb622ab76c03bf1f99702b331490410a
Henrique Conde <ext.henrique.conde@farfetch.com>
2022-09-16 19:52:47 +0100

    feat: update heading size and color options

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -5,34 +5,77 @@ import Heading from '.'
 
 describe('<Heading />', () => {
   it('should render a white heading by default', () => {
-    renderWithTheme(<Heading>Amora</Heading>)
-    expect(screen.getByRole('heading', { name: /Amora/i })).toHaveStyle({
+    renderWithTheme(<Heading>amora</Heading>)
+    expect(screen.getByRole('heading', { name: /amora/i })).toHaveStyle({
       color: '#FAFAFA'
     })
   })
 
   it('should render a black heading when color is passed', () => {
-    renderWithTheme(<Heading color="black">Amora</Heading>)
-    expect(screen.getByRole('heading', { name: /Amora/i })).toHaveStyle({
+    renderWithTheme(<Heading color="black">amora</Heading>)
+    expect(screen.getByRole('heading', { name: /amora/i })).toHaveStyle({
       color: '#030517'
     })
   })
 
   it('should render a heading with a line to the left side', () => {
-    renderWithTheme(<Heading lineLeft>Amora</Heading>)
-    expect(screen.getByRole('heading', { name: /Amora/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1'
+    renderWithTheme(<Heading lineLeft>amora</Heading>)
+    expect(screen.getByRole('heading', { name: /amora/i })).toHaveStyle({
+      'border-left': '0.7rem solid #F231A5'
     })
   })
 
   it('should render a heading with a line at the bottom', () => {
-    renderWithTheme(<Heading lineBottom>Amora</Heading>)
-    expect(screen.getByRole('heading', { name: /Amora/i })).toHaveStyleRule(
+    renderWithTheme(<Heading lineBottom>amora</Heading>)
+    expect(screen.getByRole('heading', { name: /amora/i })).toHaveStyleRule(
       'border-bottom',
       '0.5rem solid #F231A5',
       {
         modifier: '::after'
       }
     )
+  })
+
+  it('should render a heading with a small size', () => {
+    renderWithTheme(<Heading size="small">amora</Heading>)
+    expect(screen.getByRole('heading', { name: /amora/i })).toHaveStyle({
+      'font-size': '1.6rem'
+    })
+
+    expect(screen.getByRole('heading', { name: /amora/i })).toHaveStyleRule(
+      'width',
+      '3rem',
+      {
+        modifier: '::after'
+      }
+    )
+  })
+
+  it('should render a Heading with a primary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="primary" lineLeft lineBottom>
+        Lorem Ipsum
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #F231A5' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after'
+    })
+  })
+
+  it('should render a Heading with a secondary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="secondary" lineLeft lineBottom>
+        Lorem Ipsum
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #3CD3C1' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+      modifier: '::after'
+    })
   })
 })
