9baf2f5399c75f423c4174f758e52b52163ef2db
Henrique Conde <ext.henrique.conde@farfetch.com>
2022-09-17 01:13:09 +0100

    chore: install and test apollo client and graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
 import Main from 'components/Main'
 
 export default function Home() {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache()
+  })
+
+  client.query({
+    query: gql`
+      query GET_BRANDS {
+        brands {
+          data {
+            attributes {
+              name
+            }
+          }
+        }
+      }
+    `
+  })
+
   return <Main />
 }
