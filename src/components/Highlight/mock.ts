bee61587f0b4d16cc54e0569836e644b2d219bfd
Henrique Conde <ext.henrique.conde@farfetch.com>
2022-09-18 14:49:59 +0100

    chore: remove float image from highlight mock

--- src/components/Highlight/mock.ts
+++ src/components/Highlight/mock.ts
@@ -3,6 +3,5 @@ export default {
   subtitle: 'Come see the new fragrance from Elyseé',
   backgroundImage: '/img/highlight-img-1.jpg',
   buttonLabel: 'Buy now',
-  buttonLink: '/elyseé/new-fragrance',
-  floatImage: '/img/perfume-float-image-1.png'
+  buttonLink: '/elyseé/new-fragrance'
 }
