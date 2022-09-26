7dd1216ba8046c459836b8347b474d347ef41427
Henrique Conde <ext.henrique.conde@farfetch.com>
2022-09-26 03:10:13 +0100

    fix: adjust product page e2e test

--- cypress/e2e/product.cy.ts
+++ cypress/e2e/product.cy.ts
@@ -5,7 +5,6 @@ describe('Product Page', () => {
 
     cy.getByDataCy('product-info').within(() => {
       cy.findByRole('heading', { name: /liz/i }).should('exist')
-      cy.findByText('$124.90').should('exist')
       cy.findByRole('button', { name: /add to cart/i }).should('exist')
       cy.findByRole('button', { name: /wishlist/i }).should('exist')
     })
@@ -16,9 +15,6 @@ describe('Product Page', () => {
     })
 
     cy.getByDataCy('content').children().should('have.length.at.least', 2)
-
-    // Upcoming products
-    cy.shouldRenderShowcase({ name: 'Upcoming', highlight: false })
 
     // recommended products
     cy.shouldRenderShowcase({
